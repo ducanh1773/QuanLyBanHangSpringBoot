e90c171ab9900fad3b25e50a8c27463de0648c6f
ducanh1773 <anhductrannguyen170703@gmail.com>
2024-09-06 22:21:37 +0700

    main

--- src/main/java/com/example/demo/controller/admin/ProductController.java
+++ src/main/java/com/example/demo/controller/admin/ProductController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller.admin;
 
+import com.example.demo.modal.Product;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,7 +14,9 @@ public class ProductController {
 
     }
     @RequestMapping("/product/product-add")
-    public String add(){
+    public String add(Model model){
+        Product product = new Product();
+        model.addAttribute("product" , model);
         return "admin/product/add";
 
     }
--- src/main/java/com/example/demo/services/ProductServiceImpl.java
+++ src/main/java/com/example/demo/services/ProductServiceImpl.java
@@ -1,18 +1,30 @@
 package com.example.demo.services;
 
 import com.example.demo.modal.Product;
+import com.example.demo.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class ProductServiceImpl implements ProductService{
+    @Autowired
+    private ProductRepository productRepository;
     @Override
     public List<Product> getAll() {
-        return List.of();
+        return this.productRepository.findAll();
     }
 
     @Override
     public Boolean create(Product product) {
-        return null;
+        try {
+            this.productRepository.save(product);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
     @Override
