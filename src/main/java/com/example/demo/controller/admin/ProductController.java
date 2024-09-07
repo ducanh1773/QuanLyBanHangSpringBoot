8a1bcde788a375e3db8e18904790c90045c55e35
ducanh1773 <anhductrannguyen170703@gmail.com>
2024-09-07 11:30:50 +0700

    main

--- src/main/java/com/example/demo/controller/admin/ProductController.java
+++ src/main/java/com/example/demo/controller/admin/ProductController.java
@@ -1,22 +1,33 @@
 package com.example.demo.controller.admin;
 
+import com.example.demo.modal.Category;
 import com.example.demo.modal.Product;
+import com.example.demo.services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin")
 public class ProductController {
+    @Autowired
+
+    private CategoryService categoryService;
     @RequestMapping("/product")
     public String index(){
         return "admin/product/index";
 
     }
-    @RequestMapping("/product/product-add")
+    @RequestMapping("/product-add")
     public String add(Model model){
         Product product = new Product();
-        model.addAttribute("product" , model);
+        model.addAttribute("product" , product);
+        List<Category> listCate = this.categoryService.getAll();
+        model.addAttribute("listCate" , listCate);
+
         return "admin/product/add";
 
     }
